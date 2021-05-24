afbed1c586c6a683141976df00367d81861ce25b
AKJAW <akjaworski1@gmail.com>
2021-05-24 21:04:28 +0200

    Replace SettingsChanger constructor map with provider

--- features/settings/src/main/java/com/akjaw/settings/composition/SettingsModule.kt
+++ features/settings/src/main/java/com/akjaw/settings/composition/SettingsModule.kt
@@ -1,10 +1,17 @@
 package com.akjaw.settings.composition
 
+import android.content.Context
+import android.content.SharedPreferences
+import com.akjaw.core.common.data.persistance.SharedPreferencesKeys
+import com.akjaw.settings.data.SettingsRepository
+import com.akjaw.settings.data.SharedPreferencesSettingsRepository
 import com.akjaw.settings.view.SettingsScreenCreator
 import com.akjaw.settings.view.SettingsScreenCreatorImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
@@ -13,4 +20,18 @@ internal abstract class SettingsModule {
 
     @Binds
     abstract fun bindStopwatchScreenCreator(creator: SettingsScreenCreatorImpl): SettingsScreenCreator
+
+    @Binds
+    abstract fun bindSettingsRepository(settingsRepository: SharedPreferencesSettingsRepository): SettingsRepository
+
+    companion object {
+
+        @Provides
+        fun bindSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+            return context.getSharedPreferences(
+                SharedPreferencesKeys.settings,
+                Context.MODE_PRIVATE
+            )
+        }
+    }
 }
