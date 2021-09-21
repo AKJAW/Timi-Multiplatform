a5bbbc0b7dc2a1c061c9548afa7a8554bd2d0bf9
AKJAW <akjaworski1@gmail.com>
2021-09-21 19:23:25 +0200

    Add inner test class for single selection

--- features/task/detail/src/test/java/com/akjaw/details/presenter/calendar/ViewModelDaySelectionTest.kt
+++ features/task/detail/src/test/java/com/akjaw/details/presenter/calendar/ViewModelDaySelectionTest.kt
@@ -10,6 +10,7 @@ import com.soywiz.klock.DateTime
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.hasSize
@@ -41,88 +42,92 @@ class ViewModelDaySelectionTest {
         systemUnderTest = createCalendarViewModel(testCoroutineDispatcher, timestampProviderStub)
     }
 
-    @Test
-    fun `Selecting a days marks it`() = runBlocking {
-        val dayViewStateToSelect = DayViewState(21, 9, 2021)
+    @Nested
+    inner class SelectingSingleDay {
 
-        systemUnderTest.selectDay(dayViewStateToSelect)
+        @Test
+        fun `Selecting a days marks it`() = runBlocking {
+            val dayViewStateToSelect = DayViewState(21, 9, 2021)
 
-        systemUnderTest.viewState.test {
-            val resultingDay = expectItem().findDay(CURRENT_MONTH_INDEX, dayViewStateToSelect)
+            systemUnderTest.selectDay(dayViewStateToSelect)
 
-            expectThat(resultingDay).isEqualTo(
-                dayViewStateToSelect.copy(isSelected = true)
-            )
-        }
-    }
+            systemUnderTest.viewState.test {
+                val resultingDay = expectItem().findDay(CURRENT_MONTH_INDEX, dayViewStateToSelect)
 
-    @Test
-    fun `Selecting the same day twice unmarks it`() = runBlocking {
-        val dayViewStateToUnselect = DayViewState(21, 9, 2021)
-        systemUnderTest.selectDay(dayViewStateToUnselect)
-
-        systemUnderTest.selectDay(dayViewStateToUnselect)
-
-        systemUnderTest.viewState.test {
-            val resultingDay = expectItem().findDay(CURRENT_MONTH_INDEX, dayViewStateToUnselect)
-
-            expectThat(resultingDay).isEqualTo(
-                dayViewStateToUnselect.copy(isSelected = false)
-            )
-        }
-    }
-
-    @Test
-    fun `Only the last selected day is marked`() = runBlocking {
-        val lastSelectedDay = DayViewState(21, 9, 2021)
-        systemUnderTest.selectDay(DayViewState(20, 9, 2021))
-        systemUnderTest.selectDay(DayViewState(15, 9, 2021))
-
-        systemUnderTest.selectDay(lastSelectedDay)
-
-        systemUnderTest.viewState.test {
-            val currentMonthRows = expectItem().months[CURRENT_MONTH_INDEX].calendarDayRows
-            val selectedDays = currentMonthRows.flatten().filter { it.isSelected }
-
-            expectThat(selectedDays).hasSize(1)
-            expectThat(selectedDays.first()).isEqualTo(
-                lastSelectedDay.copy(isSelected = true)
-            )
-        }
-    }
-
-    @Test
-    fun `Selecting a day in the previous month works`() = runBlocking {
-        val dayViewStateToSelect = DayViewState(21, 8, 2021)
-
-        systemUnderTest.selectDay(dayViewStateToSelect)
-
-        systemUnderTest.viewState.test {
-            val resultingDay = expectItem().findDay(PREVIOUS_MONTH_INDEX, dayViewStateToSelect)
-
-            expectThat(resultingDay).isEqualTo(
-                dayViewStateToSelect.copy(isSelected = true)
-            )
-        }
-    }
-
-    @Test
-    fun `Selecting two days in different months only marks the last one`() = runBlocking {
-        val lastSelectedDay = DayViewState(21, 9, 2021)
-        systemUnderTest.selectDay(DayViewState(20, 8, 2021))
-
-        systemUnderTest.selectDay(lastSelectedDay)
-
-        systemUnderTest.viewState.test {
-            val allRows = expectItem().months.flatMap { it.calendarDayRows }
-            val allSelectedDays = allRows.flatMap { row ->
-                row.filter { day -> day.isSelected }
+                expectThat(resultingDay).isEqualTo(
+                    dayViewStateToSelect.copy(isSelected = true)
+                )
             }
+        }
 
-            expectThat(allSelectedDays).hasSize(1)
-            expectThat(allSelectedDays.first()).isEqualTo(
-                lastSelectedDay.copy(isSelected = true)
-            )
+        @Test
+        fun `Selecting the same day twice unmarks it`() = runBlocking {
+            val dayViewStateToUnselect = DayViewState(21, 9, 2021)
+            systemUnderTest.selectDay(dayViewStateToUnselect)
+
+            systemUnderTest.selectDay(dayViewStateToUnselect)
+
+            systemUnderTest.viewState.test {
+                val resultingDay = expectItem().findDay(CURRENT_MONTH_INDEX, dayViewStateToUnselect)
+
+                expectThat(resultingDay).isEqualTo(
+                    dayViewStateToUnselect.copy(isSelected = false)
+                )
+            }
+        }
+
+        @Test
+        fun `Only the last selected day is marked`() = runBlocking {
+            val lastSelectedDay = DayViewState(21, 9, 2021)
+            systemUnderTest.selectDay(DayViewState(20, 9, 2021))
+            systemUnderTest.selectDay(DayViewState(15, 9, 2021))
+
+            systemUnderTest.selectDay(lastSelectedDay)
+
+            systemUnderTest.viewState.test {
+                val currentMonthRows = expectItem().months[CURRENT_MONTH_INDEX].calendarDayRows
+                val selectedDays = currentMonthRows.flatten().filter { it.isSelected }
+
+                expectThat(selectedDays).hasSize(1)
+                expectThat(selectedDays.first()).isEqualTo(
+                    lastSelectedDay.copy(isSelected = true)
+                )
+            }
+        }
+
+        @Test
+        fun `Selecting a day in the previous month works`() = runBlocking {
+            val dayViewStateToSelect = DayViewState(21, 8, 2021)
+
+            systemUnderTest.selectDay(dayViewStateToSelect)
+
+            systemUnderTest.viewState.test {
+                val resultingDay = expectItem().findDay(PREVIOUS_MONTH_INDEX, dayViewStateToSelect)
+
+                expectThat(resultingDay).isEqualTo(
+                    dayViewStateToSelect.copy(isSelected = true)
+                )
+            }
+        }
+
+        @Test
+        fun `Selecting two days in different months only marks the last one`() = runBlocking {
+            val lastSelectedDay = DayViewState(21, 9, 2021)
+            systemUnderTest.selectDay(DayViewState(20, 8, 2021))
+
+            systemUnderTest.selectDay(lastSelectedDay)
+
+            systemUnderTest.viewState.test {
+                val allRows = expectItem().months.flatMap { it.calendarDayRows }
+                val allSelectedDays = allRows.flatMap { row ->
+                    row.filter { day -> day.isSelected }
+                }
+
+                expectThat(allSelectedDays).hasSize(1)
+                expectThat(allSelectedDays.first()).isEqualTo(
+                    lastSelectedDay.copy(isSelected = true)
+                )
+            }
         }
     }
 
