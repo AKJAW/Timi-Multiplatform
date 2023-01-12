41e1d14e1582dbaf9bd7bd9f281b2a3fa059f115
AKJAW <akjaworski1@gmail.com>
2023-01-12 19:33:09 +0100

    fix: KermitDebugLogger

--- kmp/core/core-shared/src/commonMain/kotlin/com/akjaw/timi/kmp/core/shared/logger/KermitDebugLogger.kt
+++ kmp/core/core-shared/src/commonMain/kotlin/com/akjaw/timi/kmp/core/shared/logger/KermitDebugLogger.kt
@@ -2,15 +2,13 @@ package com.akjaw.timi.kmp.core.shared.logger
 
 import co.touchlab.kermit.Logger
 
-internal class KermitDebugLogger(
-    private val logger: Logger
-) : DebugLogger {
+internal class KermitDebugLogger : DebugLogger {
 
     override fun log(tag: String, message: String) {
-        logger.withTag(tag).i(message)
+        Logger.withTag(tag).i(message)
     }
 
     override fun error(tag: String, throwable: Throwable) {
-        logger.withTag(tag).e(throwable.stackTraceToString())
+        Logger.withTag(tag).e(throwable.stackTraceToString())
     }
 }
